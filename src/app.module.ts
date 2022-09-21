71898f84443700782f9dd5c795ff86dbe56fc5c5
Alexey Klintsov <akidev@MacBook-Air-Alexey.local>
2022-09-21 16:13:19 +0300

    commit fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {TrackModule} from "./track/track.module";
+import {MongooseModule} from "@nestjs/mongoose";
+import {FileModule} from "./file/file.module";
+import * as path from 'path';
+import {ServeStaticModule} from "@nestjs/serve-static";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ServeStaticModule.forRoot({
+            rootPath: path.resolve(__dirname, 'static'),
+        }),
+        MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.r5x92.mongodb.net/music-platform-next?retryWrites=true&w=majority'),
+        TrackModule,
+        FileModule,
+    ]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import {AppModule} from './app.module';
 
 const start = async () => {
     try {
+        const PORT = process.env.PORT || 3000;
         const app = await NestFactory.create(AppModule);
-        await app.listen(3000);
+        app.enableCors();
+        await app.listen(PORT, () => console.log(`server started on PORT: ${PORT}`));
     } catch (e) {
         console.log(e);
     }
